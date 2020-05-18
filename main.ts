ad234eb81e595d56523f9841241cf53a4fe8de8f
MrWhite <hugoblanc.blend@gmail.com>
2020-05-18 23:58:39 +0200

    feat: typeorm user table created

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
 import { App, Area } from 'https://deno.land/x/alosaur/src/mod.ts';
 import { HomeController } from './src/controller/home.controller.ts';
+import { initConnexion } from './connexion.ts';
 
 
 // Declare module
 @Area({
     controllers: [HomeController],
 })
-export class HomeArea {}
+export class MainArea {}
 
 // Create alosaur application
 const app = new App({
-    areas: [HomeArea],
+    areas: [MainArea],
 });
+
+initConnexion();
 
 app.listen();
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,7 +1,11 @@
-import { Entity } from 'https://denolib.com/denolib/typeorm@v0.2.23-rc3/mod.ts';
+import { Entity, PrimaryGeneratedColumn } from 'https://denolib.com/denolib/typeorm@v0.2.23-rc3/mod.ts';
 
 
 @Entity()
 export class User{
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
 
 }
